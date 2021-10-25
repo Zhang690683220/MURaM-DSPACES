a56606201c83fe218ee86926ba23b2f154b0aebe
Zhang690683220 <zhangbo_china@live.cn>
2021-10-25 16:21:28 -0400

    adapt to lastest dspaces API

--- src/io_xysl.C
+++ src/io_xysl.C
@@ -1145,7 +1145,7 @@ void diag_output(const RunData& Run, const GridData& Grid,const PhysicsData& Phy
       sprintf(ds_var_name, "%s%s", Run.path_3D,diag_names[var]);
       clk = MPI_Wtime();
       dspaces_put_req = dspaces_iput(ds_client, ds_var_name, Run.globiter,
-                                      sizeof(float), Grid.NDIM, lb, ub, iobuf_loc);
+                                      sizeof(float), Grid.NDIM, lb, ub, iobuf_loc, 0, 0);
       dspaces_time += MPI_Wtime() - clk;
       clk = MPI_Wtime();
     }
@@ -1389,7 +1389,7 @@ void eos_output(const RunData& Run, const GridData& Grid,const PhysicsData& Phys
       sprintf(ds_var_name, "%s%s", Run.path_3D,eos_names[var]);
       clk = MPI_Wtime();
       dspaces_put_req = dspaces_iput(ds_client, ds_var_name, Run.globiter,
-                                      sizeof(float), Grid.NDIM, lb, ub, iobuf_loc);
+                                      sizeof(float), Grid.NDIM, lb, ub, iobuf_loc, 0, 0);
       dspaces_time += MPI_Wtime() - clk;
       clk = MPI_Wtime();
     }
@@ -1524,7 +1524,7 @@ void eos_output_gpu(const RunData& Run, const GridData& Grid,const PhysicsData& 
     if(ds_put_local) {
       dspaces_put_local(ds_client, ds_var_name, Run.globiter, sizeof(float), Grid.NDIM, lb, ub, dev_ptr);
     } else {
-      dspaces_iput(ds_client, ds_var_name, Run.globiter, sizeof(float), Grid.NDIM, lb, ub, dev_ptr);
+      dspaces_iput(ds_client, ds_var_name, Run.globiter, sizeof(float), Grid.NDIM, lb, ub, dev_ptr, 0, 0);
     }
     if(ds_ret != 0) {
       cout << "Error Writing " << ds_var_name << "Version: " << Run.globiter
